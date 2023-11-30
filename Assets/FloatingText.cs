134df7ac625b13e62fbede443fdb005aca2cb015
Azin A <aalahkhani@gmail.com>
2023-11-30 19:15:57 +0000

    add score audio

--- Assets/FloatingText.cs
+++ Assets/FloatingText.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class FloatingText : MonoBehaviour
 {
     private float destroyTime = 2.5f;
+    private Vector3 changeScale = new Vector3(-0.1f, 0, 0);
 
+    public AudioSource audioSource;
         void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
         Destroy(gameObject,destroyTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //gameObject.transform.localScale += changeScale;
     }
 }
