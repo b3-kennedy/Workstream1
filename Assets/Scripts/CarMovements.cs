1824029ea9dca8558b3b2dda8996ee2b80275baf
Azin A <aalahkhani@gmail.com>
2023-12-05 00:15:44 +0000

    update

--- Assets/Scripts/CarMovements.cs
+++ Assets/Scripts/CarMovements.cs
@@ -300,7 +300,7 @@ public class CarMovements : MonoBehaviour
 
 
         //  change parking conditions 
-        if (colliders.Length > 0 && moveInput <= 0.4f && thisCar.isParked == false && currentDriver!=null)
+        if (colliders.Length > 0 && moveInput == 0&& thisCar.isParked == false && currentDriver!=null)
         {
 
 
