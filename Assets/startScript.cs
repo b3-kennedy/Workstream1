e110e3aedc2a801c4d27e50f63a996ec3b882e50
b3-kennedy <benjamin3.kennedy@live.uwe.ac.uk>
2024-01-29 16:25:19 +0000

    UI is now active in level 2, start screen play button linked with level select

--- Assets/startScript.cs
+++ Assets/startScript.cs
@@ -22,6 +22,6 @@ public class startScript : MonoBehaviour
     }
     void Play() {
         Debug.Log("play pressesd");
-        SceneManager.LoadScene("w");
+        SceneManager.LoadScene(1);
     }
 }
